a3b0fc74e1801d5a6fdbe2155b5b1bf57255a31e
Administrator <admin@example.com>
2020-04-07 15:42:55 +0100

    Added ExceptionHandler annotacion

--- src/main/java/com/ergodic/friends/controller/ControllerExceptionHandler.java
+++ src/main/java/com/ergodic/friends/controller/ControllerExceptionHandler.java
@@ -1,18 +1,27 @@
 package com.ergodic.friends.controller;
 
 import com.ergodic.friends.util.ErrorMessage;
+import com.ergodic.friends.util.FieldErrorMessage;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.xml.bind.ValidationException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
     @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler
-    ErrorMessage exceptionHandler(ValidationException e){
-        return new ErrorMessage("400", e.getMessage());
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    List<FieldErrorMessage> exceptionHandler(MethodArgumentNotValidException e){
+        List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+        List<FieldErrorMessage> fieldErrorMessages = fieldErrors.stream().map(fieldError ->
+                new FieldErrorMessage(fieldError.getField(), fieldError.getDefaultMessage())).collect(Collectors.toList());
+
+        return fieldErrorMessages;
     }
 }
--- src/main/java/com/ergodic/friends/controller/FriendController.java
+++ src/main/java/com/ergodic/friends/controller/FriendController.java
@@ -3,13 +3,18 @@ package com.ergodic.friends.controller;
 import com.ergodic.friends.model.Friend;
 import com.ergodic.friends.service.FriendService;
 import com.ergodic.friends.util.ErrorMessage;
+import com.ergodic.friends.util.FieldErrorMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.xml.bind.ValidationException;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 public class FriendController {
@@ -18,8 +23,16 @@ public class FriendController {
     FriendService friendService;
 
     @PostMapping("/friend")
-    Friend create (@RequestBody Friend friend){
+    Friend create (@RequestBody Friend friend) throws ValidationException {
+        if (friend.getId() == 0 && friend.getFirstName() != null && friend.getLastName() != null)
             return friendService.save(friend);
+        else throw new ValidationException("friend cannot be created");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    ErrorMessage exceptionHandler(ValidationException e){
+        return new ErrorMessage("400", e.getMessage());
     }
 
     @GetMapping("/friend")
